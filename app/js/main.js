43013d92abfb3b1339097b9c6fd85d17a4e3446d
Korchin Maxim <98269691+korchinmv@users.noreply.github.com>
2023-05-02 21:37:25 +0300

    Delete app directory

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-// const buttonMenu = document.querySelector('.burger');
-// const menuActive = document.querySelector('.menu');
-
-// buttonMenu.onclick = function () {
-// 	menuActive.classList.toggle('menu--active');
-// 	buttonMenu.classList.toggle('burger--active');
-// };
