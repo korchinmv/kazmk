24c6f2690dab2901110958348cf7ed7dcfd11cec
korchinmv <korchinmv@yandex.ru>
2023-05-04 16:02:47 +0300

    added: header, footer, hero -  adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
-// const buttonMenu = document.querySelector('.burger');
-// const menuActive = document.querySelector('.menu');
+const buttonMenu = document.querySelector(".burger");
+const menuActive = document.querySelector(".header__menu");
 
-// buttonMenu.onclick = function () {
-// 	menuActive.classList.toggle('menu--active');
-// 	buttonMenu.classList.toggle('burger--active');
-// };
+buttonMenu.addEventListener("click", () => {
+  menuActive.classList.toggle("header__menu--active");
+  buttonMenu.classList.toggle("burger--active");
+});
